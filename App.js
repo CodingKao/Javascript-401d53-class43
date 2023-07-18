7f8c0b0ea8900e009efc3eaa5a7d4a3e82952e95
Kao Saelor <saelor.kao@gmail.com>
2023-07-17 23:50:47 -0700

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { Text, View, Button, Platform, TextInput } from 'react-native';
+import * as Notifications from 'expo-notifications';
+import * as Calendar from 'expo-calendar';
 
 export default function App() {
+  const [billName, setBillName] = useState('');
+  const [amount, setAmount] = useState('');
+  const [dueDate, setDueDate] = useState('');
+  const [expoPushToken, setExpoPushToken] = useState('');
+
+  const scheduleReminder = async () => {
+    const { status } = await Calendar.requestCalendarPermissionsAsync();
+    if (status === 'granted') {
+      const calendars = await Calendar.getCalendarsAsync();
+      const defaultCalendar = calendars.find((calendar) => calendar.source.name === 'Default');
+      if (defaultCalendar) {
+        const event = {
+          title: billName,
+          startDate: new Date(dueDate),
+          endDate: new Date(dueDate),
+          timeZone: 'GMT',
+        };
+        const { id } = await Calendar.createEventAsync(defaultCalendar.id, event);
+        console.log(`Event created with ID: ${id}`);
+        setBillName('');
+        setAmount('');
+        setDueDate('');
+      }
+    }
+  };
+
+  useEffect(() => {
+    const registerForPushNotificationsAsync = async () => {
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+      }
+      const token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log(token);
+      setExpoPushToken(token);
+    };
+
+    registerForPushNotificationsAsync();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Bill Name:</Text>
+      <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginBottom: 10 }}
+        onChangeText={(text) => setBillName(text)}
+        value={billName}
+      />
+      <Text>Amount:</Text>
+      <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginBottom: 10 }}
+        onChangeText={(text) => setAmount(text)}
+        value={amount}
+      />
+      <Text>Due Date:</Text>
+      <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginBottom: 10 }}
+        onChangeText={(text) => setDueDate(text)}
+        value={dueDate}
+      />
+      <Button title="Add Bill" onPress={scheduleReminder} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ README.md
@@ -0,0 +1,35 @@
+# Javascript-401d53-class43
+
+## Learning React Native using:
+
+![Expo](https://img.shields.io/badge/expo-1C1E24?style=for-the-badge&logo=expo&logoColor=#D04A37) 
+![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB) 
+![React Native](https://img.shields.io/badge/react_native-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB) 
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%010101) 
+
+
+## Problem Domain
+
+Bill Payment Reminder: A React Native app that utilizes calendar and push notification features to help users manage bill payments efficiently.
+
+### Author: Kao Saelor
+
+### LAB - React Native - Milestone 1: MVP
+
+**Lab/Coding Session #1** - Focus on getting your dev system up and operational, getting "proof of life" and reaching your MVP in terms of baseline functionality
+
+**Lab/Coding Session #2** - Add in the device features, polish, and prepare for deployments.
+
+### Requirements
+
+- Use at least 1 native device feature (Contacts, Camera, GPS, etc)
+- High Fidelity Styling and UX
+- Properly Documented for End Users
+- Live Demo & Presentation
+
+### Milestone 1 - MVP
+
+- Deliver your MVP for raw functionality
+- Your dev environment should be properly working
+- Other users should be able to scan your QR and see your app on any device
+- All of your core "features" should be working properly
